17801d92208ea1d0286e649cebb03e0eebe30e9c
manishr2411 <manish.r2411@gmail.com>
2026-09-04 00:14:54 +0530

    Make training dataset path portable

--- Source_code/Person3_AI/train_custom.py
+++ Source_code/Person3_AI/train_custom.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     model = YOLO("yolo11n.pt")
 
     model.train(
-        data=r"C:\Users\Admin\OneDrive\Desktop\Hackathon_Custom_YOLO\roboflow_dataset\data.yaml",
+        data="data.yaml"
         epochs=50,
         imgsz=640,
         batch=8
